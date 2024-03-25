ec36be531e53b6063a38820a474d384fcb936514
may <may@ap-com.co.jp>
2024-03-25 21:14:15 +0900

    try to fix error

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import gradio as gr
 #    "huggingface/google/vit-base-patch16-224",
 #    examples=["./images/cat.jpg", "./images/man.jpg"]).launch()
 
+
 #Webアプリ内で実行する関数を定義
 def greet(name):
     return "Hello " + name + "!"
@@ -12,4 +13,4 @@ demo = gr.Interface(fn=greet,      #Webアプリ内で実行する関数
                     inputs="text", #関数への入力値
                     outputs="text")#関数の出力値
 
-demo.launch()  
+demo.launch(share=True)  
